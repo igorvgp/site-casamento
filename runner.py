27228a06bcd9e3b6ca412b8177b42b7a5eee9b5b
igorvgp <igorviniciusgpereira@gmail.com>
2024-12-12 22:07:16 -0300

    set environ variables

--- runner.py
+++ runner.py
@@ -6,19 +6,29 @@ from google.oauth2.service_account import Credentials
 import os
 import base64
 import pandas as pd
+from pathlib import Path
 
 local_path = os.getcwd()
 
+# Configurar as credenciais do Google Sheets a partir do Streamlit Secrets
+creds_json = st.secrets["google"]["creds"]
+
+# Salvar credenciais temporariamente (necessário para gspread)
+temp_path = Path("temp_credentials.json")
+with open(temp_path, "w") as f:
+    f.write(creds_json)
+
 # Configurar as credenciais do sheets
-credentials_file = os.path.join(local_path, "settings", "credentials.json")
+#redentials_file = os.path.join(local_path, "settings", "credentials.json")
 
 scopes = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
-credentials = Credentials.from_service_account_file(credentials_file, scopes=scopes)
+
+credentials = Credentials.from_service_account_file(temp_path, scopes=scopes)
+#credentials = Credentials.from_service_account_file(credentials_file, scopes=scopes)
 client = gspread.authorize(credentials)
-print("OKKKKK")
 
 # Abrir a planilha
 spreadsheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1Fy8dVCIIAeElyKrw3TYwhgqoygGxyWWg0dEI6Um4AZk/edit?usp=sharing")
