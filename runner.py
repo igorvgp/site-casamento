c725e919d886fb737c6e1a165c8c321bc4239664
igorvgp <igorviniciusgpereira@gmail.com>
2025-01-03 00:06:17 -0300

    fixed runner credentials

--- runner.py
+++ runner.py
@@ -18,18 +18,19 @@ scopes = [
 ]
 
 # Configurar as credenciais do Google Sheets a partir do Streamlit Secrets
-# creds_json = st.secrets["google"]["creds"]
+creds_json = st.secrets["google"]["creds"]
 
-# # Salvar credenciais temporariamente (necessário para gspread)
-# temp_path = Path("temp_credentials.json")
-# with open(temp_path, "w") as f:
-#     f.write(creds_json)
+# Salvar credenciais temporariamente (necessário para gspread)
+temp_path = Path("temp_credentials.json")
+with open(temp_path, "w") as f:
+    f.write(creds_json)
 
-# credentials = Credentials.from_service_account_file(temp_path, scopes=scopes)
+credentials = Credentials.from_service_account_file(temp_path, scopes=scopes)
 
 # Configurar as credenciais do sheets
-credentials_file = os.path.join(local_path, "settings", "credentials.json")
-credentials = Credentials.from_service_account_file(credentials_file, scopes=scopes)
+# credentials_file = os.path.join(local_path, "settings", "credentials.json")
+# credentials = Credentials.from_service_account_file(credentials_file, scopes=scopes)
+
 client = gspread.authorize(credentials)
 
 # Abrir a planilha
